fc3904975e3bfe46722475e09bd8d87c6817fd9e
Chuck Smith <chuck@eclecticsaddlebag.com>
2020-08-03 18:55:50 -0400

    Migrate to Postgres

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200722214601) do
+ActiveRecord::Schema.define(version: 2020_07_22_214601) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "todos", force: :cascade do |t|
     t.string "name"
